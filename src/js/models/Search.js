8d0d02bf79b7818c757b186b5915fea06add8c2d
Dorota Michniak <dorota.michniak@essentialskillz.com>
2018-10-23 19:22:03 +0100

    initial commit

---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults(query) {
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '94981561a17874fe39f6a462857d67ee';
+        try {
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            //console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
