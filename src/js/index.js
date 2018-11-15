f5e6eaa79693042d60d49cae473b7bfa07a76494
Dorota Michniak <dorota.michniak@essentialskillz.com>
2018-11-15 19:05:31 +0000

    buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from './models/Search';
 import * as searchView from './views/searchView';
-import { elements } from './views/base';
+import { elements, renderLoader, clearLoader } from './views/base';
 
 /* Global stat of the app
  * - Search object 
@@ -18,21 +18,30 @@ const controlSearch = async() => {
     if (query) {
         // 2. New search object and add to state
         state.search = new Search(query);
-
     }
-
     // 3. Prepare UI for results
     searchView.clearInput();
     searchView.clearResults();
+    renderLoader(elements.searchRes);
 
     // 4. Search for recipes
     await state.search.getResults();
 
     // 5. Render results on UI
+    clearLoader();
     searchView.renderResults(state.search.result);
 }
 
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
 });
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -6,10 +6,10 @@ export default class Search {
     }
 
     async getResults(query) {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        // const proxy = 'https://cors-anywhere.herokuapp.com/';
         const key = '94981561a17874fe39f6a462857d67ee';
         try {
-            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
             //console.log(this.result);
         } catch (error) {
